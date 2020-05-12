3dc7524e6617f4d14f1cf4cae18239bd5321f8a2
Kanakamedala Sirisha <63331131+KanakamedalaSiri@users.noreply.github.com>
2020-05-12 19:12:17 +0530

    Update MappingOfDigitsToLetters.cpp

--- Case_4/MappingOfDigitsToLetters.cpp
+++ Case_4/MappingOfDigitsToLetters.cpp
@@ -60,17 +60,21 @@ void MappingOfLetters::letterCombinations(int iDigits[], int iNum)
     
 } 
 
-int main() 
+int main(int argc,char* argv[]) 
 {
-    MappingOfLetters obj;
-    int iNum;
-    cout<<"Enter num of elements "<<endl;
-    cin>>iNum;
-    int iDigits[iNum]; 
-    cout<<"Enter elements"<<endl;
-    for(int iIndex=0;iIndex<iNum;iIndex++)
-    cin>>iDigits[iIndex];
-    obj.letterCombinations(iDigits, iNum); 
-  
-    return 0; 
+    if (argc==2 && strcmp(argv[1],"-h")==0)
+    cout<<"Usage:"<<argv[0]<<"Positive Integeres between 2-9"<<endl;
+    else
+    {
+        MappingOfLetters obj;
+        int iNum;
+        cout<<"Enter num of elements "<<endl;
+        cin>>iNum;
+        int iDigits[iNum]; 
+        cout<<"Enter elements"<<endl;
+        for(int iIndex=0;iIndex<iNum;iIndex++)
+        cin>>iDigits[iIndex];
+        obj.letterCombinations(iDigits, iNum);
+        }
+        return 0; 
 } 
