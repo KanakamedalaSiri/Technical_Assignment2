89b0c966be88de80e91430dc74955d8d735ed8ab
Kanakamedala Sirisha <63331131+KanakamedalaSiri@users.noreply.github.com>
2020-05-25 19:11:01 +0530

    Update Pch.h

--- Case_3/Pch.h
+++ Case_3/Pch.h
@@ -14,16 +14,18 @@ class EncodeDecode
     std::string sNewFile;
     int iStartPos;
     int iEndPos;
-    bool EncodeFile(std::string sfileName,std::string sKey);
+    
     std::string GenerateFileName(std::string sfileName,std::string sToReplace,std::string sToUpdate);
     std::string GetKey(std::string sKey);
     bool WriteEncodeFile(std::string sfileName,std::string sNewFile,std::string sKey,std::string sAlphaNum);
-    bool DecodeFile(std::string sfileName,std::string sKey);
+    
     
     public:
     EncodeDecode();
     ~EncodeDecode();
-    bool SecuredFile(char** cArgVar);
+    bool EncodeFile(std::string sfileName,std::string sKey);
+    bool DecodeFile(std::string sfileName,std::string sKey);
+    
     
 };
     
