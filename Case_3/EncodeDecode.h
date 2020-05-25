a40486063f3850d223cd682fedc09f6e3ce9a190
Kanakamedala Sirisha <63331131+KanakamedalaSiri@users.noreply.github.com>
2020-05-25 23:56:26 +0530

    Update EncodeDecode.h

--- Case_3/EncodeDecode.h
+++ Case_3/EncodeDecode.h
@@ -8,17 +8,12 @@ class EncodeDecode
     private:
     std::string sWordInFile;
     std::string sLineInFile;
-    std::string sTemp;
     std::string sToReplace;
     std::string sToUpdate;
     std::string sNewFile;
-    int iStartPos;
-    int iEndPos;
-    
     std::string GenerateFileName(std::string sfileName,std::string sToReplace,std::string sToUpdate);
     std::string GetKey(std::string sKey);
     bool WriteEncodeFile(std::string sfileName,std::string sNewFile,std::string sKey,std::string sAlphaNum);
-    
     
     public:
     EncodeDecode();
